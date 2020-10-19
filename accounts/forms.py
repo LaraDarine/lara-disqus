888b1310d9072824acf7b41063bccf652bb312ed
LaraDarine <delara.risal2017@gmail.com>
2020-10-19 22:30:35 +0100

    Create signup form :sparkles:

---
+++ accounts/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django import forms
+
+
+User = get_user_model()
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+    email = forms.EmailField(max_length=256, help_text='Email required')
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2',
+        )
