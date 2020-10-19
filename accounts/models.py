a6cb28bf8b79560a47802131a095ce1d89d80f70
LaraDarine <delara.risal2017@gmail.com>
2020-10-19 21:51:38 +0100

    Create Profile model :clown_face:

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='accounts/', null=True)
+
+    def __str__(self):
+        return self.user.username
