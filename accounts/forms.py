ae2f593379afec54ad10b806ec9ba4648b85b68f
LaraDarine <delara.risal2017@gmail.com>
2020-10-19 22:32:56 +0100

    Create user's picture form :camera_flash:

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django import forms
+from .models import Profile
 
 
 User = get_user_model()
@@ -21,3 +22,11 @@ class SignUpForm(UserCreationForm):
             'password1',
             'password2',
         )
+
+
+class UserPictureForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = (
+            'picture',
+            )
