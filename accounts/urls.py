ff72f53cfcb865197522da9424a22c66b00c31ca
LaraDarine <delara.risal2017@gmail.com>
2020-10-20 21:29:33 +0100

    Add activate email url :heavy_plus_sign:

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import signup
+from .views import signup, activate_email
 
 
 urlpatterns = [
     path('accounts/signup/', signup, name='signup'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/activate/<user_id>/<token>/', activate_email, name='activate_email'),
 ]
