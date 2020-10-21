20f434b71a6dc0cb2c4fdd1ba13ba42f1b54afa7
LaraDarine <delara.risal2017@gmail.com>
2020-10-21 19:23:59 +0100

    Add profile url :man:

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import signup, activate_email
+from .views import signup, activate_email, profile
 
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/activate/<user_id>/<token>/', activate_email, name='activate_email'),
+    
+    path('accounts/profile/', profile, name='profile'),
 ]
