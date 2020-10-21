8cb199d76b0f1249c93dfabf0ddba3bb1115fa1b
LaraDarine <delara.risal2017@gmail.com>
2020-10-21 19:52:41 +0100

    Create disucussion model :thought_balloon:

--- discussions/models.py
+++ discussions/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from topics.models import Topic
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Discussion(models.Model):
+    topic = models.ForeignKey(
+        Topic,
+        related_name='topic',
+        on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User,
+        related_name='author',
+        on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(
+        upload_to='discussions/',
+        default='discussions/discussion.png')
+
+
+    def __str__(self):
+        return f'{self.author.username} - {self.topic.title} - {self.created_at}'
+    
+
+    def get_comments(self):
+        from comments.models import Comment
+        comments = Comment.objects.filter(post=self)
+        return comments
