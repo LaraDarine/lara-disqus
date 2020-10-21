14d98511f2a6c126e3a529c0836e77c666946b75
LaraDarine <delara.risal2017@gmail.com>
2020-10-21 19:44:10 +0100

    Create topic model :thought_balloon:

--- topics/models.py
+++ topics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='topics/', default='topics/topic.png')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
