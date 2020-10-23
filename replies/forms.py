0b7c7cd43736a38b91ab8e893ff896ef3c06d6cf
LaraDarine <delara.risal2017@gmail.com>
2020-10-23 22:41:10 +0100

    Create reply form :speech_balloon:

---
+++ replies/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django import forms
+from .models import Reply
+
+
+User = get_user_model()
+
+
+class ReplyForm(forms.ModelForm):
+
+    class Meta:
+        model = Reply
+        fields = (
+            'content',
+        )
+    
+    def add_comment(self, user, comment):
+        self.instance.author = user
+        self.instance.comment = comment
+        self.instance.save()
