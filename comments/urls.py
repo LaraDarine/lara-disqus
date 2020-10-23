399b2e50a643708c667265a6c8f0f6d664beeb1d
LaraDarine <delara.risal2017@gmail.com>
2020-10-23 16:42:44 +0100

    Fix urls & comment votes attribute to distinguish btwn disc & cmnt :bug:

--- comments/urls.py
+++ comments/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import comments, comment_details, toggle_like, toggle_dislike
+from .views import comments, comment_details, toggle_comment_like, toggle_comment_dislike
 
 
 urlpatterns = [
     path('comments/', comments, name='comments'),
     path('comments/<int:pk>/', comment_details, name='comment_details'),
-    path('comments/<int:pk>/likes', toggle_like, name='toggle_like'),
-    path('comments/<int:pk>/dislikes', toggle_dislike, name='toggle_dislike'),
+    path('comments/<int:pk>/likes', toggle_comment_like, name='toggle_comment_like'),
+    path('comments/<int:pk>/dislikes', toggle_comment_dislike, name='toggle_comment_dislike'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -24,31 +24,30 @@ def comment_details(request, pk):
     return render(request, 'comments/comment-details.html', context)
 
 #TODO: Make this view func globally
-def toggle_like(request, pk):
+def toggle_comment_like(request, pk):
     current_user = request.user
     if current_user.is_authenticated:
         comment = get_object_or_404(Comment, pk=pk)
-        print(comment.comment_likes)
         
-        if comment.comment_likes.filter(id=current_user.id).exists():
-            comment.comment_likes.remove(current_user.id)
+        if comment.likes.filter(id=current_user.id).exists():
+            comment.likes.remove(current_user.id)
         else:
-            comment.comment_likes.add(current_user.id)
-            if comment.comment_dislikes.filter(id=current_user.id).exists():
-                comment.comment_dislikes.remove(current_user.id)
+            comment.likes.add(current_user.id)
+            if comment.dislikes.filter(id=current_user.id).exists():
+                comment.dislikes.remove(current_user.id)
         return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
 
-def toggle_dislike(request, pk):
+def toggle_comment_dislike(request, pk):
     current_user = request.user
     if current_user.is_authenticated:
         comment = get_object_or_404(Comment, pk=pk)
-        print(comment.comment_dislikes)
+        print(comment.dislikes)
         
-        if comment.comment_dislikes.filter(id=current_user.id).exists():
-            comment.comment_dislikes.remove(current_user.id)
+        if comment.dislikes.filter(id=current_user.id).exists():
+            comment.dislikes.remove(current_user.id)
         else:
-            comment.comment_dislikes.add(current_user.id)
-            if comment.comment_likes.filter(id=current_user.id).exists():
-                comment.comment_likes.remove(current_user.id)
+            comment.dislikes.add(current_user.id)
+            if comment.likes.filter(id=current_user.id).exists():
+                comment.likes.remove(current_user.id)
 
         return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
