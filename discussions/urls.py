0fb19a380be65044a600ed625bc0d28773fda8d7
LaraDarine <delara.risal2017@gmail.com>
2020-10-23 11:36:23 +0100

    Create toggle dislikes url for test :-1:

--- discussions/urls.py
+++ discussions/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import discussions, toggle_like
+from .views import discussions, toggle_like, toggle_dislike
 
 
 urlpatterns = [
     path('discussions/', discussions, name='discussions'),
     path('discussions/<int:pk>/likes', toggle_like, name='toggle_like'),
+    path('discussions/<int:pk>/dislikes', toggle_dislike, name='toggle_dislike'),
 ]
