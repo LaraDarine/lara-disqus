5f031772b3058e71166a0bc0acdd65d7acee39b3
LaraDarine <delara.risal2017@gmail.com>
2020-10-23 15:04:33 +0100

    Create Comment model :tada:

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from discussions.models import Discussion
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Comment(models.Model):
+    discussion = models.ForeignKey(
+        Discussion,
+        related_name='discussion',
+        on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User,
+        related_name='comment_author',
+        on_delete=models.CASCADE)
+    content = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(
+        upload_to='comments/',
+        blank=True
+        )
+    likes = models.ManyToManyField(
+        User,
+        related_name='comment_likes',
+        blank=True
+        )
+    dislikes = models.ManyToManyField(
+        User,
+        related_name='comment_dislikes',
+        blank=True
+        )
+
+    def __str__(self):
+        return f'{self.author.username}-{self.created_at}'
+
+    def likes_count(self):
+        return self.likes.count()
+
+    def dislikes_count(self):
+        return self.dislikes.count()
