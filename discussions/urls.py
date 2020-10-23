8d5aa6a58e5e13c0f5a21dbfa3e02687019fc6a9
LaraDarine <delara.risal2017@gmail.com>
2020-10-23 14:16:08 +0100

    Create discussion details url :speech_balloon: :tada:

--- discussions/urls.py
+++ discussions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import discussions, toggle_like, toggle_dislike
+from .views import discussions, discussion_details, toggle_like, toggle_dislike
 
 
 urlpatterns = [
     path('discussions/', discussions, name='discussions'),
+    path('discussions/<int:pk>/', discussion_details, name='discussion_details'),
     path('discussions/<int:pk>/likes', toggle_like, name='toggle_like'),
     path('discussions/<int:pk>/dislikes', toggle_dislike, name='toggle_dislike'),
 ]
