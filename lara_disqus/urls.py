a4640f14035442570403d2515401bcd6dd9e5ca2
LaraDarine <delara.risal2017@gmail.com>
2020-10-24 17:49:02 +0100

    Set home page url direction :house:

--- lara_disqus/urls.py
+++ lara_disqus/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
 
 
+def home(request):
+    return redirect('discussions')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('', include('accounts.urls')),
     path('', include('comments.urls')),
     path('', include('discussions.urls')),
