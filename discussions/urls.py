5f4cbe9a04b5c7c531f7cd24227601f6af3050f5
LaraDarine <delara.risal2017@gmail.com>
2020-10-25 06:36:46 +0100

    Create delele_coment url :wastebasket:

--- discussions/urls.py
+++ discussions/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import discussions, discussion_details, toggle_like, toggle_dislike, home
+from .views import discussions, discussion_details, toggle_like, toggle_dislike, home, delete_comment
 
 
 urlpatterns = [
     path('discussions/welcome', home, name='home'),
     path('discussions/', discussions, name='discussions'),
     path('discussions/<int:pk>/', discussion_details, name='discussion_details'),
+    path('discussions/comment/<int:pk>/delete', delete_comment, name='delete_comment'),
     path('discussions/<int:pk>/likes', toggle_like, name='toggle_like'),
     path('discussions/<int:pk>/dislikes', toggle_dislike, name='toggle_dislike'),
 ]
