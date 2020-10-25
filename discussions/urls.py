bcffb3cd246fe0a9e567d4d6f630dacf2eee4b64
LaraDarine <delara.risal2017@gmail.com>
2020-10-25 17:50:27 +0100

    Add edit_comment url :pencil2: :speech_balloon:

--- discussions/urls.py
+++ discussions/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import discussions, discussion_details, toggle_like, toggle_dislike, home, delete_comment
+from .views import discussions, discussion_details, toggle_like, toggle_dislike, home, delete_comment, edit_comment
 
 
 urlpatterns = [
@@ -7,6 +7,7 @@ urlpatterns = [
     path('discussions/', discussions, name='discussions'),
     path('discussions/<int:pk>/', discussion_details, name='discussion_details'),
     path('discussions/comment/<int:pk>/delete', delete_comment, name='delete_comment'),
+    path('discussions/comment/<int:pk>/edit', edit_comment, name='edit_comment'),
     path('discussions/<int:pk>/likes', toggle_like, name='toggle_like'),
     path('discussions/<int:pk>/dislikes', toggle_dislike, name='toggle_dislike'),
 ]
