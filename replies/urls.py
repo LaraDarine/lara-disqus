8b724403fd2859271cfd553cb7158d1bd4c278e1
LaraDarine <delara.risal2017@gmail.com>
2020-11-03 20:09:00 +0100

    Delete replies app :wastebasket:

--- replies/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import replies, reply_details, toggle_reply_like, toggle_reply_dislike
-
-
-urlpatterns = [
-    path('replies/<int:pk>/', reply_details, name='reply_details'),
-    path('replies/<int:pk>/likes', toggle_reply_like, name='toggle_reply_like'),
-    path('replies/<int:pk>/dislikes', toggle_reply_dislike, name='toggle_reply_dislike'),
-]
