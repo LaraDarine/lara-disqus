503cc0c5febd7bfc6fea38d6472f79d7d8331df6
LaraDarine <delara.risal2017@gmail.com>
2020-11-05 05:22:35 +0100

    Add auth views urls :passport_control:

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,9 +5,23 @@ from .views import signup, activate_email, profile
 
 urlpatterns = [
     path('accounts/signup/', signup, name='signup'),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('accounts/activate/<user_id>/<token>/', activate_email, name='activate_email'),
-    
+    path('accounts/login/', auth_views.LoginView.as_view(),
+        name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(),
+        name='logout'),
+    path('accounts/activate/<user_id>/<token>/',
+        activate_email, name='activate_email'),
+    path('accounts/password-reset',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'),
+    path('accounts/password-reset/done',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'),
+    path('accounts/password-reset/confirm/<uidb64>/<token>',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
+    path('accounts/password-reset/complete',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'),
     path('accounts/profile/', profile, name='profile'),
 ]
