99bdcb87076f942d196963d09ff2c866309f0411
LaraDarine <delara.risal2017@gmail.com>
2020-11-05 06:24:30 +0100

    Clean up the code,  Add slahes at the end of urls :bath:

--- comments/urls.py
+++ comments/urls.py
@@ -5,6 +5,6 @@ from .views import comments, comment_details, toggle_comment_like, toggle_commen
 urlpatterns = [
     path('comments/', comments, name='comments'),
     path('comments/<int:pk>/', comment_details, name='comment_details'),
-    path('comments/<int:pk>/likes', toggle_comment_like, name='toggle_comment_like'),
-    path('comments/<int:pk>/dislikes', toggle_comment_dislike, name='toggle_comment_dislike'),
+    path('comments/<int:pk>/likes/', toggle_comment_like, name='toggle_comment_like'),
+    path('comments/<int:pk>/dislikes/', toggle_comment_dislike, name='toggle_comment_dislike'),
 ]
--- discussions/urls.py
+++ discussions/urls.py
@@ -3,13 +3,13 @@ from .views import discussions, discussion_details, toggle_like, toggle_dislike,
 
 
 urlpatterns = [
-    path('discussions/welcome', home, name='home'),
+    path('discussions/welcome/', home, name='home'),
     path('discussions/', discussions, name='discussions'),
     path('discussions/<int:pk>/', discussion_details, name='discussion_details'),
-    path('discussions/comment/<int:pk>/delete', delete_comment, name='delete_comment'),
-    path('discussions/comment/<int:pk>/edit', edit_comment, name='edit_comment'),
-    path('discussions/<int:pk>/likes', toggle_like, name='toggle_like'),
-    path('discussions/<int:pk>/dislikes', toggle_dislike, name='toggle_dislike'),
-    path('discussions/<int:pk>/dislikes', toggle_dislike, name='toggle_dislike'),
-    path('discussions/toggle_order', toggle_order, name='toggle_order'),
+    path('discussions/comment/<int:pk>/delete/', delete_comment, name='delete_comment'),
+    path('discussions/comment/<int:pk>/edit/', edit_comment, name='edit_comment'),
+    path('discussions/<int:pk>/likes/', toggle_like, name='toggle_like'),
+    path('discussions/<int:pk>/dislikes/', toggle_dislike, name='toggle_dislike'),
+    path('discussions/<int:pk>/dislikes/', toggle_dislike, name='toggle_dislike'),
+    path('discussions/toggle_order/', toggle_order, name='toggle_order'),
 ]
